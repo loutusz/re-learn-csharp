f45d53cf7d76c0a1cfb179b65552bd867b8e5c73
loutusz <j1008.id@gmail.com>
2026-06-04 14:23:08 +0700

    logic exercise 1

---
+++ LogicExercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write("foobar");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write("foo");
+    } 
+    else if (i % 5 == 0)
+    {
+        Console.Write("bar");
+    } 
+    else
+    {
+        Console.Write(i);
+    }
+
+    if (i != number)
+    {
+        Console.Write(", ");
+    }
+}
